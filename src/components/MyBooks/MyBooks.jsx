368e5c3dfcc27b603b81645b5f43422955c461a2
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-01-28 14:58:58 +0100

    add

--- src/components/MyBooks/MyBooks.jsx
+++ src/components/MyBooks/MyBooks.jsx
@@ -1,27 +1,39 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+
 import styles from './my-books.module.scss';
+import ContactList from './ContactList/ContactList';
+import ContactFilter from './ContactFilter/ContactFilter';
+// import items from 'components/items';
+import ContactForm from './ContactForm/ContactForm';
 
 class MyBooks extends Component {
   state = {
-    items: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    name: '',
-    number: '',
+    items: [],
     filter: '',
   };
 
-  addContact = e => {
-    e.preventDefault();
-    const { name, number, items } = this.state;
+  componentDidMount() {
+    const items = JSON.parse(localStorage.getItem('my-contact'));
+    if (items?.length) {
+      //items && items.length
+      this.setState({ items });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { items } = this.state;
+    if (prevState.items.length !== items.length) {
+      localStorage.setItem('my-contact', JSON.stringify(items));
+    }
+  }
+
+  addContact = ({ name, number }) => {
     if (this.isDublicate(name, number)) {
       return alert(`${name} tel.${number} is already in contacts`);
     }
     this.setState(prevState => {
+      const { items } = prevState;
       const newContact = {
         id: nanoid(),
         name,
@@ -31,12 +43,16 @@ class MyBooks extends Component {
     });
   };
 
-  removeContact(id) {
+  removeContact = id => {
     this.setState(({ items }) => {
-      const newContact = items.filter(item => item.id !== id);
-      return { items: newContact };
+      const newContacts = items.filter(item => item.id !== id);
+      return { items: newContacts };
     });
-  }
+  };
+
+  handleFilter = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
 
   isDublicate(name, number) {
     const normalizedName = name.toLowerCase();
@@ -50,13 +66,6 @@ class MyBooks extends Component {
     });
     return Boolean(contact);
   }
-
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  };
 
   getVisibleContacts() {
     const { filter, items } = this.state;
@@ -74,51 +83,25 @@ class MyBooks extends Component {
   }
 
   render() {
-    const { addContact, handleChange } = this;
-    const { name, number } = this.state;
-    const items = this.getVisibleContacts();
-    const contacts = items.map(({ id, name, number }) => (
-      <li key={id}>
-        {name} tel. {number}{' '}
-        <button onClick={() => this.removeContact(id)}>Delete</button>
-      </li>
-    ));
+    const { addContact, handleFilter, removeContact } = this;
+    const contacts = this.getVisibleContacts();
+
     return (
       <div>
         <h3>Phonebook</h3>
         <div className={styles.wrapper}>
           <div className={styles.block}>
             <h4>Add new contact</h4>
-            <form action="" onSubmit={addContact}>
-              <div className={styles.formGroup}>
-                <label>Name</label>
-                <input
-                  name="name"
-                  value={name}
-                  onChange={handleChange}
-                  placeholder="Name"
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <label>Tel.number</label>
-                <input
-                  name="number"
-                  value={number}
-                  onChange={handleChange}
-                  placeholder="tel.number"
-                />
-              </div>
-              <button type="submit">Add contact</button>
-            </form>
+            <ContactForm onSubmit={addContact} />
           </div>
           <div className={styles.block}>
-            <h4>Contacts:</h4>
-            <h4>All Contacts:</h4>
-            <div className={styles.formGroup}>
-              <label>Find contact</label>
-              <input name="filter" onChange={handleChange} placeholder="tel" />
-            </div>
-            <ol>{contacts}</ol>
+            <h4 className={styles.titleContacts}>Contacts:</h4>
+            <h4 className={styles.allContacts}>
+              All contacts: {contacts.length}
+            </h4>
+            <ContactFilter handleChange={handleFilter} />
+
+            <ContactList removeContact={removeContact} items={contacts} />
           </div>
         </div>
       </div>
